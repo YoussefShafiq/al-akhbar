a885a7bd503f22fb1f091608d14b1a2918f7fba4
youssef shafek <youssefmsa616@gmail.com>
2025-12-30 13:21:53 +0200

    faqs, summary, related news, responsive updates

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -30,3 +30,35 @@ const swiper = new Swiper('.mostReadSwiper', {
         },
     },
 });
+
+const relatedNews = new Swiper('.relatedNews', {
+    slidesPerView: 1,
+    spaceBetween: 20,
+    loop: true,
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: '.swiper-pagination-custom',
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next-custom',
+        prevEl: '.swiper-button-prev-custom',
+    },
+    breakpoints: {
+        640: {
+            slidesPerView: 1,
+            spaceBetween: 20,
+        },
+        1024: {
+            slidesPerView: 2,
+            spaceBetween: 24,
+        },
+        1280: {
+            slidesPerView: 3,
+            spaceBetween: 24,
+        },
+    },
+});
